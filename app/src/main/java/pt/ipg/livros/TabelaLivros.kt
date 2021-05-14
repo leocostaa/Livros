b4b183eef558d684bb442da51b228f9dfc45fb33
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-05-14 10:56:14 +0100

    Criar tabela de livros

--- app/src/main/java/pt/ipg/livros/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/livros/TabelaLivros.kt
@@ -10,12 +10,13 @@ class TabelaLivros (db: SQLiteDatabase) {
 
     fun cria() {
         db.execSQL("CREATE TABLE" + NOME_TABELA + "(" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                BaseColumns._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
                 CAMPO_TITULO + "TEXT NOT NULL," +
                 CAMPO_AUTOR + "TEXT NOT NULL," +
                 CAMPO_ID_CATEGORIA + "INTEGER NOT NULL," +
                 "FOREIGN KEY (" + CAMPO_ID_CATEGORIA + ")" +
-                     "REFERENCES" + TabelaCategorias.NOME_TABELA )
+                     "REFERENCES" + TabelaCategorias.NOME_TABELA +
+                ")")
 
 
         //CREATE TABLE livros (
