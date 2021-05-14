50a6c00c9221148dc91559dc1b37846deace26b2
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-05-14 10:09:03 +0100

    Criada classe para aceder a tabela livros

--- app/src/main/java/pt/ipg/livros/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/livros/TabelaLivros.kt
@@ -5,13 +5,27 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
-class TabelaCategorias(db: SQLiteDatabase) {
+class TabelaLivros (db: SQLiteDatabase) {
     private val db : SQLiteDatabase = db
 
     fun cria() {
         db.execSQL("CREATE TABLE" + NOME_TABELA + "(" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                CAMPO_NOME + "TEXT NOT NULL)")
+                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CAMPO_TITULO + "TEXT NOT NULL," +
+                CAMPO_AUTOR + "TEXT NOT NULL," +
+                CAMPO_ID_CATEGORIA + "INTEGER NOT NULL," +
+                "FOREIGN KEY (" + CAMPO_ID_CATEGORIA + ")" +
+                     "REFERENCES" + TabelaCategorias.NOME_TABELA
+        )
+                
+
+        //CREATE TABLE livros (
+        //      _id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //      titulo TEXT NOT NULL,
+        //      autor TEXT NOT NULL,
+        //      id_categoria INTEGER NOT NULL,
+        //      FOREIGN KEY (id_categoria) REFERENCES categorias
+        //      )
 
     }
 
@@ -39,7 +53,13 @@ class TabelaCategorias(db: SQLiteDatabase) {
     }
 
     companion object{
-        const val NOME_TABELA = "categorias"
-        const val CAMPO_NOME = "nome"
+        const val NOME_TABELA = "livros"
+        const val CAMPO_TITULO = "titulo"
+        const val CAMPO_AUTOR = "autor"
+        const val CAMPO_ID_CATEGORIA = "id_categoria"
+
+        //categoria          livro
+        //          1------N
     }
 }
+
